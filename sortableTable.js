4aa39a0a569acaae90cb748e078ed65d04d9c3e4
aeronka <Natalja-055@yandex.ru>
2015-11-27 20:26:37 +0300

    Добавлено переключение по страницам таблицы

--- sortableTable.js
+++ sortableTable.js
@@ -6,15 +6,24 @@ export default class SortableTable {
         this.tbody = options.tableBodyElem;
         this.forSmallData = options.forSmallData;
         this.forBigData = options.forBigData;
+        this.rowsPerPage = options.rowsPerPage;
 
         this.header = document.querySelector('.content-header');
+        this.pageNav = document.querySelector('.page-navigation');
+        this.pageNumberElem = this.pageNav.querySelector('.page-number');
+
         //выбор загрузки большого или маленького кол-ва данных
         this.header.addEventListener('change', event => {
             //спрятать и удалить таблицу,если она уже была показана
             this.hideElements();
             let url = event.target.matches('.small-data') ? this.forSmallData : this.forBigData;
+            //обнуляем текущую страницу из массива диапазонов
+            this.currentPage = 0;
             this.query(url);
         });
+
+        //переключение между страницами
+        this.pageNav.addEventListener('click', event => this.changePage(event));
     }
 
     showElements() {
@@ -40,20 +49,24 @@ export default class SortableTable {
                 return response.json();
             })
             .then(function(response) {
+                this.data = response;
                 //добавить пришедшие данные в таблицу и показать её
-                this.addTable(response);
+                this.addTable();
             }.bind(this))
             .catch(function(error) {
                 throw error;
             });
-    }   
+    }
 
-    addTable(data) {
+    addTable() {
+        //диапазон страниц в зависимости от текущей страницы
+        let range = this.splitPage();
         let output = '';
 
-        for(let i = 0; i < data.length; i++) {
-            output += Mustache.render(templ.contacts, data[i]);
+        for(let i = range.first; i <= range.last; i++) {
+            output += Mustache.render(templ.contacts, this.data[i]);
         }
+
         this.tbody.insertAdjacentHTML('beforeEnd', output);
         this.showElements();
     }
@@ -63,6 +76,53 @@ export default class SortableTable {
         table.removeChild(this.tbody);
         this.tbody = document.createElement('tbody');
         table.appendChild(this.tbody);
+    }
+
+    //разделение по страницам
+    splitPage() {
+        //количество страниц
+        this.pageNumbers = Math.ceil(this.data.length / this.rowsPerPage);
+        //массив диапазонов
+        let ranges = [];
+        let firstValue = 0;
+
+        for(let i = 0; i < this.pageNumbers; i++) {
+            ranges.push( { first: firstValue , last: firstValue + (this.rowsPerPage - 1) } );
+            firstValue = firstValue + 1 + (this.rowsPerPage - 1);
+        }
+
+        //проверка номера последней строки последнего диапазона, для случаев если последняя страница не полностью заполнена
+        if(ranges[this.pageNumbers - 1].last > (this.data.length - 1)) ranges[this.pageNumbers - 1].last = this.data.length - 1;
+
+        //возвращает диапазон показа текущей страницы
+        return ranges[this.currentPage];
+    }
+
+    changePage(event) {
+        if(event.target.matches('.page-right')) {
+            this.goPageMore();
+        } else {
+            this.goPageLess();
+        };
+
+        this.removeTable();
+        this.addTable();
+    }
+
+    goPageMore() {
+        this.currentPage = this.currentPage + 1;
+        //проверка максимального значения количества страниц
+        if(this.currentPage + 1 > this.pageNumbers) this.currentPage = this.pageNumbers - 1;
+
+        this.pageNumberElem.innerHTML = this.currentPage + 1;
+    }
+
+    goPageLess() {
+        this.currentPage = this.currentPage - 1;
+        //проверка минимального значения количества страниц
+        if(this.currentPage < 0) this.currentPage = 0;
+
+        this.pageNumberElem.innerHTML = this.currentPage + 1;
     }
 
 }
